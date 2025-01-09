edb91b34832d819f0109373e614627e095e0f3d1
Rasio09 <rasiovolume22@gmail.com>
2025-01-09 21:25:57 +0700

    upload proses

---
+++ python/index.py
@@ -0,0 +1,36 @@
+gretting = 'hello world!!'
+print(gretting)
+
+additional = 2+2
+result = additional - 1
+print(result)
+
+for i in range(2):
+    print(i)
+
+for i in range(1,3):
+    print(i)
+
+for i in range(1,10,2):
+    print(i)
+
+
+i=1
+while i <= 5:
+    print(i)
+    i+=1
+
+
+for i in range(1, 5):
+    if i%2 == 0:
+        print(i, "= genap")
+
+# input
+# nama = str(input("Masukkan nama kamu : "))
+# print(nama)
+
+
+# TO DO KUIS VARIABEL
+
+gretting = "Saya mulai belajar python"
+print(gretting)
