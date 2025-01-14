886785aa476a377bd6ee3482d0aa753344bf3ba7
Rasio09 <rasiovolume22@gmail.com>
2025-01-14 20:27:54 +0700

    upload proses lagi 3

--- python/index.py
+++ python/index.py
@@ -509,3 +509,310 @@ result = nilai/len(var_array)
 print(result)
 
 
+# MATRIKS
+matriks = [[0 for j in range(4)] for i in range(3)]
+print(matriks)
+
+# Membuat matriks 2x2
+var_mat = [[5, 0],
+          [1, -2]]
+def_mat = [[0 for j in range(2)] for i in range(2)]
+
+for i in range(len(var_mat)):
+  for j in range(len(var_mat[0])):
+    def_mat[i][j] = var_mat[i][j]*2
+
+print(def_mat)
+
+
+# METHOD ATAU FUNGSI
+def mencari_luas_persegi_panjang(panjang,lebar):
+    luas_persegi_panjang = panjang*lebar
+    return luas_persegi_panjang
+
+persegi_panjang_pertama = mencari_luas_persegi_panjang(5,10)
+print(persegi_panjang_pertama)
+
+persegi_panjang_kedua = mencari_luas_persegi_panjang(4,15)
+print(persegi_panjang_kedua)
+
+# FUNGSI DENGAN TUPLE
+# JIKA PARAMETER ADA / MAKA ARGUMEN TANPA NAMA VARIABEL, JIKA PARAMETER ADA * MAKA ARGUMEN DENGAN NAMA VARIABEL
+def hitung_total(*args):
+    print(type(args))
+    total = sum(args)
+    return total
+
+print(hitung_total(1, 2, 3))
+
+# FUNGSI DENGAN DICTIONARY KEY AND VALUE
+def cetak_info(**kwargs):
+    info = ""
+    for key, value in kwargs.items():
+        info += key + ': ' + value + ", "
+    return info
+
+print(cetak_info(nama="Dicoding", usia="17", pekerjaan="Python Programmer"))
+
+# FUNGSI DENGAN LAMDA EXSPRESION
+# dari ini: 
+# def func(args):
+#     return ret_val
+
+# berubah jadi ini :
+# func = lambda args: ret_val
+
+mencari_luas_persegi_panjang = lambda panjang, lebar: panjang*lebar
+print(mencari_luas_persegi_panjang(5,10))
+
+
+# KUISS
+"""
+TODO:
+Buatlah sebuah fungsi bernama "minimal" dengan ketentuan berikut.
+- Menerima dua buah argumen berupa number, yaitu a dan b.
+- Mengembalikan nilai terkecil antara a atau b.
+- Bila nilai keduanya sama, kembalikan dengan nilai a.
+"""
+
+# TODO: Silakan buat kode Anda di bawah ini.
+def minimal(a,b):
+    if a>b :
+      return b
+    elif a==b :
+      return a
+    else :
+        return a
+  
+cetak = minimal(5,3)
+print(cetak)
+
+# fungsi
+# def perhitungan(a,b):
+#     nilai = a * 2
+#     nilai2 = b * 2
+#     jumlah = nilai+nilai2
+#     return print("jumlahnya adalah %i" % (jumlah))
+
+# inputA = int(input("masukkan nilai A"))
+# inputB = int(input("masukkan nilai B"))
+# perhitungan(inputA, inputB)
+
+# OOP
+class Mobil:
+    # Atribut
+    warna = "Merah"
+
+mobil_1 = Mobil()
+print(mobil_1.warna)
+
+# class construtor agar jika merubah atribut nilainya juga tidak ikut berubah
+class Mobil:
+    # Atribut Instance
+    def __init__(self):
+        self.warna = 'Merah'
+        
+mobil_1 = Mobil()
+mobil_2 = Mobil()
+
+print(mobil_1.warna)
+print(mobil_2.warna)
+
+# Mengubah atribut instance
+mobil_1.warna = "Hitam"
+
+# Menampilkan kembali atribut warna
+print(mobil_1.warna)
+print(mobil_2.warna)
+
+
+class Mobil:
+    def __init__(self, warna, merek, kecepatan):
+        self.warna = warna
+        self.merek = merek
+        self.kecepatan = kecepatan
+        
+mobil_1 = Mobil('Merah', 'DicodingCar', 160)
+
+print(mobil_1.warna)
+print(mobil_1.merek)
+print(mobil_1.kecepatan)
+
+# DEKORATOR
+def my_decorator(func):
+    def wrapper():
+        print("Sebelum fungsi dieksekusi.")
+        func()
+        print("Setelah fungsi dieksekusi.")
+    return wrapper
+
+# Dekorasi fungsi dengan decorator
+@my_decorator
+def say_hello():
+    print("Hello, world!")
+
+# Memanggil fungsi yang sudah didekorasi
+say_hello()
+
+# METODE DARI OBJEK
+class Mobil:
+    def __init__(self, warna, merek, kecepatan):
+        self.warna = warna
+        self.merek = merek
+        self.kecepatan = kecepatan
+
+    def tambah_kecepatan(self):
+        self.kecepatan += 10
+
+mobil_1 = Mobil("Merah", "DicodingCar", 160)
+print("Sebelum ditambahkan: ")
+print(mobil_1.kecepatan)
+
+mobil_1.tambah_kecepatan()        # Memanggil metode tambah_kecepatan.
+
+print("Setelah ditambahkan: ")
+print(mobil_1.kecepatan)
+
+
+# METODE SECARA STATIS
+class Mobil:
+    def __init__(self, merek):
+        self.merek = merek
+    
+    @staticmethod
+    def intro_mobil():
+        print("Ini adalah metode dari kelas Mobil")
+        
+Mobil.intro_mobil()
+mobil_1 = Mobil("DicodingCar")
+mobil_1.intro_mobil()
+
+
+# METODE DARI KELAS
+class Mobil:
+    def __init__(self, merek):
+        self.merek = merek
+
+    @classmethod
+    def intro_mobil(cls):
+        print("Ini adalah metode dari kelas Mobil")
+        
+Mobil.intro_mobil()
+mobil_1 = Mobil("DicodingCar")
+mobil_1.intro_mobil()
+
+# INHERITANCE (PEWARISAN)
+class Mobil:
+    def __init__(self, warna, merek, kecepatan):
+        self.warna = warna
+        self.merek = merek
+        self.kecepatan = kecepatan
+    
+    def tambah_kecepatan(self):
+        self.kecepatan += 10
+
+mobil_1 = Mobil("Merah", "DicodingCar", 160)
+print(mobil_1.kecepatan)
+mobil_1.tambah_kecepatan()
+print(mobil_1.kecepatan)
+
+class MobilSport(Mobil):
+    def turbo(self):
+        self.kecepatan += 50
+
+# Kelas Mobil Sport
+mobil_sport_1 = MobilSport("Hitam", "DicodingSportCar", 160)
+print(mobil_sport_1.kecepatan)
+mobil_sport_1.turbo()
+print(mobil_sport_1.kecepatan)
+
+# OVERRIDE
+class Mobil:
+    def __init__(self, warna, merek, kecepatan):
+        self.warna = warna
+        self.merek = merek
+        self.kecepatan = kecepatan
+    
+    def tambah_kecepatan(self):     # tambah_kecepatan
+        self.kecepatan += 10
+
+class MobilSport(Mobil):
+    def turbo(self):
+        self.kecepatan += 50
+    
+    def tambah_kecepatan(self):     # tambah_kecepatan
+        self.kecepatan += 20
+
+# Kelas Mobil Sport
+mobil_sport_1 = MobilSport("Hitam", "DicodingSportCar", 160)
+print(mobil_sport_1.kecepatan)
+mobil_sport_1.tambah_kecepatan()     # Memanggil metode baru tambah kecepatan()
+print(mobil_sport_1.kecepatan)
+print(mobil_sport_1.warna)
+
+
+# SUPER
+class Mobil:
+    def __init__(self, warna, merek, kecepatan):
+        self.warna = warna
+        self.merek = merek
+        self.kecepatan = kecepatan
+    
+    def tambah_kecepatan(self):
+        self.kecepatan += 10
+
+
+class MobilSport(Mobil):
+    def turbo(self):
+        self.kecepatan += 50
+    
+    def tambah_kecepatan(self):
+        super().tambah_kecepatan()
+        print("Kecepatan Anda meningkat! Hati-Hati!")
+
+# Kelas Mobil Sport
+mobil_sport_1 = MobilSport("Hitam", "DicodingSportCar", 160)
+mobil_sport_1.tambah_kecepatan()     # Memanggil metode baru tambah kecepatan()
+print(mobil_sport_1.kecepatan)
+
+# KUISS
+"""
+TODO:
+1. Buatlah class bernama Animal dengan ketentuan:
+    - Memiliki properti:
+      - name: string
+      - age: int
+      - species: string
+    - Memiliki constructor untuk menginisialisasi properti:
+      - name
+      - age
+      - species
+2. Buatlah class bernama Cat dengan ketentuan:
+    - Merupakan turunan dari class Animal
+    - Memiliki method:
+      - bernama "deskripsi" yang mengembalikan nilai string berikut ini.
+        "{self.name} adalah kucing berjenis {self.species} yang sudah berumur {self.age} tahun"
+      - bernama "suara" yang akan mengembalikan nilai string "meow!"
+ 3. Buatlah instance dari kelas Cat bernama "myCat" dengan ketentuan:
+    - Atribut name bernilai: "Neko"
+    - Atribut age bernilai: 3
+    - Atribut species bernilai: "Persian".
+"""
+
+#TODO: Silakan buat kode Anda di bawah ini.
+class Animal:
+    def __init__(self, name, age, species):
+        self.name = name
+        self.age = age
+        self.species = species
+
+class Cat(Animal):
+    def deskripsi(self):
+        return f"{self.name} adalah kucing berjenis {self.species} yang sudah berumur {self.age} tahun"
+    
+    def suara(self):
+        return "meow!"
+
+myCat = Cat("Neko", 3, "Persian")
+print(myCat.deskripsi())
+print(myCat.name)
